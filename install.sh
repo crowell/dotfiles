9a1217e6796abbc670d3e7490329cb4bdc21b158
Jeffrey Crowell <jeff@crowell.biz>
2023-07-02 20:14:10 -0400

    make sure that there is a ~/.config/nvim directory

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ ln -s `pwd`/tmux.conf ~/.tmux.conf
 ln -s `pwd`/vimrc ~/.vimrc
 ln -s `pwd`/radare2rc ~/.radare2rc
 ln -s `pwd`/gitconfig ~/.gitconfig
+mkdir -p ~/.config/nvim
 ln -s `pwd`/init.vim ~/.config/nvim/init.vim
 ln -s `pwd`/wezterm.lua ~/.wezterm.lua
 ln -s `pwd`/zshrc ~/.zshrc
